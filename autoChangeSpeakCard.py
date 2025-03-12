7633cb3a14d17311d4d4e72f08088fdcb2d2d865
lambert <Lambert_work@163.com>
2025-03-12 23:32:04 +0800

    Add files via upload

--- autoChangeSpeakCard.py
+++ autoChangeSpeakCard.py
@@ -11,7 +11,9 @@ from pydub.playback import play
 from threading import Thread, Lock
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pathlib import Path
 import cv2
+import numpy as np
 import simpleaudio as sa
 import os
 import time
@@ -22,6 +24,7 @@ import random
 import requests
 import json
 import shutil
+
 lock = Lock()
 # flag
 t = 0.5
@@ -194,7 +197,7 @@ def operating(driver_handler, driver_speaker):
             print("播放讲解卡标题——脚本运行结束")
             try:
                 good_video_duration = 0
-                video_path = f"{data['videos_path']}/{good_index}.mp4"
+                video_path = f"{data['videos_path']}/{now_good_id}/{now_good_id}.mp4"
                 if os.path.exists(video_path):
                     x = Thread(target=play_good_video, args=(video_path,))
                     x.start()
@@ -251,6 +254,62 @@ def wait_for_audio_completion(driver, params, timeout=30):
     driver.execute_script(
         "window.audioPlaybackCompleted=false;window.speakCardCompleted = false;")
 
+
+# 播放图片
+def play_good_img(img_url):
+    image_folder = img_url
+    images = [img for img in os.listdir(image_folder) if img.endswith(
+        ".png") or img.endswith(".jpg") or img.endswith(".jpeg")]
+    if not images:
+        print("没有找到图片")
+    else:
+        cv2.namedWindow("Image Slideshow", cv2.WINDOW_AUTOSIZE)
+        target_width = 400
+        aspect_ratio = 16 / 9.0
+        target_height = int(target_width * (16 / 9))
+        index = 0
+        while True:
+            img_path = os.path.join(image_folder, images[index])
+            img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+            if img is None:
+                print(f"无法加载图像 {img_path}")
+                index += 1
+                if index >= len(images):
+                    break
+                continue
+            original_height, original_width = img.shape[:2]
+            ratio = target_width / float(original_width)
+            new_height = int(original_height * ratio)
+            if new_height > target_height:
+                ratio = target_height / float(original_height)
+                new_height = target_height
+                target_width_temp = int(original_width * ratio)
+            else:
+                target_width_temp = target_width
+            img_resized = cv2.resize(
+                img, (target_width_temp, new_height), interpolation=cv2.INTER_AREA)
+            background_color = [0, 255, 0]
+            final_img = np.full((target_height, target_width, 3),
+                                background_color, dtype=np.uint8)
+            y_offset = int((target_height - new_height) / 2)
+            x_offset = int((target_width - target_width_temp) / 2)
+            if img_resized.shape[2] == 4:
+                alpha_channel = img_resized[:, :, 3] / 255.0
+                for c in range(0, 3):
+                    final_img[y_offset:y_offset + new_height, x_offset:x_offset + target_width_temp, c] = \
+                        alpha_channel * img_resized[:, :, c] + (
+                            1 - alpha_channel) * final_img[y_offset:y_offset + new_height, x_offset:x_offset + target_width_temp, c]
+            else:
+                final_img[y_offset:y_offset + new_height,
+                          x_offset:x_offset + target_width_temp, :] = img_resized
+            cv2.imshow("Image Slideshow", final_img)
+            if cv2.waitKey(2500) & 0xFF == ord('q'):
+                break
+            index += 1
+            if index >= len(images):
+                break
+        cv2.destroyAllWindows()
+
 # 异步播放视频
 
 
@@ -258,6 +317,9 @@ def play_good_video(video_url):
     global is_good_video_play, good_video_duration
     is_play_audio = False
     cap = cv2.VideoCapture(video_url)
+    # 设置目标宽度和长宽比
+    target_width = 400
+    aspect_ratio = 9 / 16
     audio = AudioSegment.from_file(video_url, format="mp4")
     now_audio = audio - 35
     raw_data = now_audio.raw_data
@@ -280,15 +342,9 @@ def play_good_video(video_url):
         if not ret or is_good_video_play == False:
             break
 
-        height, width = frame.shape[:2]
-        new_width = 600
-        new_height = int(new_width * height / width)
-        resized_frame = cv2.resize(
-            frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
-
-        current_time = time.time() - start_time
-        frame_number = int(current_time * fps)
-        cv2.imshow("Video", resized_frame)
+        # 调整帧大小并添加绿色填充以符合目标尺寸
+        processed_frame = resize_and_pad(frame, target_width, aspect_ratio)
+        cv2.imshow("Video", processed_frame)
         if is_play_audio == False:
             audio_play_obj = sa.play_buffer(
                 raw_data,
@@ -339,6 +395,41 @@ def auto_send_comment(driver):
 
 def type_character(element, text):
     element.send_keys(text)
+
+
+def resize_and_pad(frame, target_width, aspect_ratio):
+    """根据目标宽度和长宽比调整图像大小，并在周围填充绿色"""
+    original_height, original_width = frame.shape[:2]
+
+    # 计算目标高度以匹配给定的宽高比
+    target_height = int(target_width / aspect_ratio)
+
+    # 计算缩放比例
+    scale_ratio = target_width / original_width
+
+    new_height = int(original_height * scale_ratio)
+
+    # 调整大小
+    resized_frame = cv2.resize(
+        frame, (target_width, new_height), interpolation=cv2.INTER_LANCZOS4)
+
+    # 创建一个绿色背景的画布
+    canvas = np.zeros((target_height, target_width, 3), dtype=np.uint8)
+    canvas[:] = (0, 255, 0)  # 绿色填充
+
+    # 如果新高度小于目标高度，则垂直居中；否则水平居中
+    if new_height < target_height:
+        y_offset = (target_height - new_height) // 2
+        x_offset = 0
+    else:
+        y_offset = 0
+        x_offset = (target_width - resized_frame.shape[1]) // 2
+
+    # 将调整后的帧放到绿色背景上
+    canvas[y_offset:y_offset + new_height, x_offset:x_offset +
+           resized_frame.shape[1]] = resized_frame[:, :target_width]
+
+    return canvas
 
 #  发送评论内容
 
@@ -426,34 +517,45 @@ def juliangbaiying_login():
     time.sleep(1)
     driver.switch_to.window(driver.window_handles[-1])
     driver.get(data["url-juliangbaiying-control"])
-    if not os.path.exists(data['videos_path']):
-        print(f"目录 {data['videos_path']} 不存在")
-        for item in range(len(data["goods"])):
-            id = data["goods"][item]["promotion_id"]
-            url = f"https://buyin.jinritemai.com/dashboard/merch-picking-library/merch-promoting?id={id}"
-            driver.switch_to.window(driver.window_handles[1])
-            time.sleep(0.2)
-            driver.execute_script("window.open();")
-            time.sleep(0.2)
-            driver.switch_to.window(driver.window_handles[-1])
-            time.sleep(0.2)
-            driver.get(url)
-            time.sleep(1)
-            try:
-                elements = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.TAG_NAME, "video"))
-                )
-            except Exception:
-                print("第 "+str(item) + " 没有商品视频")
-                driver.close()
-                driver.switch_to.window(driver.window_handles[1])
-                continue
+    # 下载视频
+    for item in range(len(data["goods"])):
+        id = data["goods"][item]["promotion_id"]
+        dir_path = Path(f"{data['videos_path']}/{id}")
+        if dir_path.exists() and dir_path.is_dir():
+            continue
+        url = f"https://buyin.jinritemai.com/dashboard/merch-picking-library/merch-promoting?id={id}"
+        driver.switch_to.window(driver.window_handles[1])
+        time.sleep(0.2)
+        driver.execute_script("window.open();")
+        time.sleep(0.2)
+        driver.switch_to.window(driver.window_handles[-1])
+        time.sleep(0.2)
+        driver.get(url)
+        time.sleep(1)
+        try:
+            # 下载视频
+            elements = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.TAG_NAME, "video"))
+            )
             time.sleep(1)
             if elements:
                 video_src = elements.get_attribute("src")
-                # data["goods"][item]["video_url"] = src
-                download_file(video_src, data['videos_path']+"/"+str(item)+".mp4")
+                download_file(
+                    video_src, data['videos_path']+f"/{id}/{id}.mp4")
+        except Exception:
+            # 下载图片
+            elements = WebDriverWait(driver, 5).until(
+                EC.presence_of_all_elements_located(
+                    (By.CLASS_NAME, "index_module__item___ac928"))
+            )
+            for item1 in range(len(elements)):
+                img_src = elements[item1].get_attribute("src")
+                download_file(
+                    img_src, data['videos_path']+f"/{id}/{item1}.jpg")
+        finally:
             driver.close()
+            driver.switch_to.window(driver.window_handles[1])
+    print("商品信息")
     print("巨量应用 加载成功")
     return driver
 
